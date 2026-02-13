e11a80e634b6a21b71dfcfdf2f2caffce431de67
abhijith-zero <abhijithrajesh18@gmail.com>
2026-02-13 00:15:07 +0000

    Init setup

---
+++ src/main/java/com/example/finaceRag/FinaceRagApplication.java
@@ -0,0 +1,13 @@
+package com.example.finaceRag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaceRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaceRagApplication.class, args);
+	}
+
+}
